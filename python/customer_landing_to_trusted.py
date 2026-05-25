a1b46b0de35037f31fd63f240938d0405b5bc9b2
Christopher Ackerman <52364404+ackerman-chris@users.noreply.github.com>
2026-05-25 16:41:08 -0500

    Update customer_landing_to_trusted.py

--- python/customer_landing_to_trusted.py
+++ python/customer_landing_to_trusted.py
@@ -4,62 +4,43 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsgluedq.transforms import EvaluateDataQuality
 from awsglue import DynamicFrame
 
-# Initialize Glue context
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
-
-# Helper function for SQL transforms
 def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
     for alias, frame in mapping.items():
         frame.toDF().createOrReplaceTempView(alias)
     result = spark.sql(query)
     return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
 
-# Read customer landing data from S3
-CustomerLanding_node = glueContext.create_dynamic_frame.from_options(
-    format_options={"multiline": False},
-    connection_type="s3",
-    format="json",
-    connection_options={
-        "paths": ["s3://YOUR-BUCKET-NAME/customer/landing/"],
-        "recurse": True,
-    },
-    transformation_ctx="CustomerLanding_node",
-)
-
-# SQL Query: Filter only customers who agreed to share data for research
-SqlQuery = """
-SELECT *
-FROM customer_landing
-WHERE sharewithresearchasofdate IS NOT NULL
+# Default ruleset used by all target nodes with data quality enabled
+DEFAULT_DATA_QUALITY_RULESET = """
+    Rules = [
+        ColumnCount > 0
+    ]
 """
 
-CustomerFiltered_node = sparkSqlQuery(
-    glueContext,
-    query=SqlQuery,
-    mapping={"customer_landing": CustomerLanding_node},
-    transformation_ctx="CustomerFiltered_node",
-)
+# Script generated for node Customer Landing
+CustomerLanding_node1779742264235 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-lakehouse-13055707/customer/landing/"], "recurse": True}, transformation_ctx="CustomerLanding_node1779742264235")
 
-# Write to S3 Trusted Zone
-CustomerTrusted_node = glueContext.getSink(
-    path="s3://YOUR-BUCKET-NAME/customer/trusted/",
-    connection_type="s3",
-    updateBehavior="UPDATE_IN_DATABASE",
-    partitionKeys=[],
-    enableUpdateCatalog=True,
-    transformation_ctx="CustomerTrusted_node",
-)
-CustomerTrusted_node.setCatalogInfo(
-    catalogDatabase="stedi", catalogTableName="customer_trusted"
-)
-CustomerTrusted_node.setFormat("json")
-CustomerTrusted_node.writeFrame(CustomerFiltered_node)
+# Script generated for node Filter by Research Opt-in
+SqlQuery0 = '''
+SELECT *
+     FROM customer_landing
+     WHERE sharewithresearchasofdate IS NOT NULL
+'''
+FilterbyResearchOptin_node1779742521101 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"customer_landing":CustomerLanding_node1779742264235}, transformation_ctx = "FilterbyResearchOptin_node1779742521101")
 
+# Script generated for node Customer Trusted
+EvaluateDataQuality().process_rows(frame=FilterbyResearchOptin_node1779742521101, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1779742259364", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+CustomerTrusted_node1779742532659 = glueContext.getSink(path="s3://stedi-lakehouse-13055707/customer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="CustomerTrusted_node1779742532659")
+CustomerTrusted_node1779742532659.setCatalogInfo(catalogDatabase="stedi",catalogTableName="customer_trusted")
+CustomerTrusted_node1779742532659.setFormat("json")
+CustomerTrusted_node1779742532659.writeFrame(FilterbyResearchOptin_node1779742521101)
 job.commit()
